ae2130331936ed242468cae65a0f820b2445a168
Zachary Kohnen <me@dusterthefirst.com>
2026-07-03 22:48:17 +0200

    Scaffolding for google auth

--- token-manager-server/src/auth/google.rs
+++ token-manager-server/src/auth/google.rs
@@ -1,50 +1,157 @@
+use std::sync::Arc;
+
+use anyhow::Context as _;
 use axum::{
-    extract::State,
+    extract::{Query, State},
+    http::StatusCode,
     response::{IntoResponse, Redirect},
 };
 use axum_extra::extract::cookie::{Cookie, PrivateCookieJar, SameSite};
 use oauth2::{
-    Client, EmptyExtraTokenFields, EndpointNotSet, EndpointSet, RevocationErrorResponseType,
-    StandardErrorResponse, StandardRevocableToken, StandardTokenIntrospectionResponse,
-    StandardTokenResponse,
-    basic::{BasicErrorResponseType, BasicTokenType},
+    AuthUrl, AuthorizationCode, CsrfToken, EndpointNotSet, EndpointSet, RedirectUrl, RevocationUrl,
+    TokenResponse as _, TokenUrl, basic::BasicClient,
 };
+use serde_derive::{Deserialize, Serialize};
+use time::Timestamp;
 
-pub async fn start_google_oauth(
+use crate::{ClientIdSecret, RouterState, TokenState};
+
+#[derive(Clone, Debug)]
+pub struct GoogleOauthClient(
+    Arc<BasicClient<EndpointSet, EndpointNotSet, EndpointNotSet, EndpointSet, EndpointSet>>,
+);
+
+pub fn oauth_client(
+    client_info: ClientIdSecret,
+    oauth_root: &str,
+) -> anyhow::Result<GoogleOauthClient> {
+    Ok(GoogleOauthClient(Arc::new(
+        BasicClient::new(client_info.client_id)
+            .set_client_secret(client_info.client_secret)
+            .set_auth_uri(
+                AuthUrl::new("https://accounts.google.com/o/oauth2/auth".to_string())
+                    .expect("auth url should be a valid url"),
+            )
+            .set_token_uri(
+                TokenUrl::new("https://oauth2.googleapis.com/token".to_string())
+                    .expect("token url should be a valid url"),
+            )
+            .set_revocation_url(
+                RevocationUrl::new("https://oauth2.googleapis.com/revoke".to_string())
+                    .expect("revocation url should be a valid url"),
+            )
+            .set_redirect_uri(
+                RedirectUrl::new(format!("{oauth_root}/oauth/google"))
+                    .context("redirect url should be a valid url")?,
+            ),
+    )))
+}
+
+const COOKIE_NAME: &str = "oauth_state";
+
+pub async fn start_youtube_oauth(
+    State(RouterState { google_oauth, .. }): State<RouterState>,
     jar: PrivateCookieJar,
-    State(oauth_client): State<
-        Client<
-            StandardErrorResponse<BasicErrorResponseType>,
-            StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
-            StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>,
-            StandardRevocableToken,
-            StandardErrorResponse<RevocationErrorResponseType>,
-            EndpointSet,
-            EndpointNotSet,
-            EndpointNotSet,
-            EndpointNotSet,
-            EndpointSet,
-        >,
-    >,
 ) -> impl IntoResponse {
-    // 1. Generate your secure random state string
-    let state_string = hex::encode(rand::random_iter().take(64).collect::<Vec<_>>());
+    let (google_url, csrf_token) = google_oauth
+        .0
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(oauth2::Scope::new(
+            "https://www.googleapis.com/auth/youtube.readonly".to_string(),
+        ))
+        .add_scope(oauth2::Scope::new(
+            "https://www.googleapis.com/auth/youtube".to_string(),
+        ))
+        // The following 2 parameters ask for a refresh token
+        .add_extra_param("access_type", "offline")
+        .add_extra_param("prompt", "consent")
+        .url();
 
-    // 2. Build the cookie explicitly
-    let state_cookie = Cookie::build(("oauth_state", state_string))
+    let state_cookie = Cookie::build((COOKIE_NAME, csrf_token.into_secret()))
         .path("/")
         .http_only(true)
-        .secure(true) // Ensure this is true if accessing over HTTPS/Tailscale
+        .secure(true)
         .same_site(SameSite::Lax)
-        // Set a short expiration! They only need 5 minutes to log in.
         .max_age(time::Duration::minutes(5))
         .build();
 
-    // 3. Add the cookie to the jar (this handles the ChaCha20 encryption automatically)
-    let updated_jar = jar.add(state_cookie);
+    (jar.add(state_cookie), Redirect::to(google_url.as_str()))
+}
 
-    // 4. Return the updated jar AND the redirect
-    let google_url = oauth_client.authorize_url(|| state_string);
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum OAuthRedirectQueryParams {
+    Success {
+        code: AuthorizationCode,
+        state: CsrfToken,
+    },
+    Failure {
+        error: String,
+    },
+}
 
-    (updated_jar, Redirect::to(&google_url))
+pub async fn complete_youtube_oauth(
+    State(RouterState {
+        google_oauth,
+        reqwest_client,
+        ..
+    }): State<RouterState>,
+    jar: PrivateCookieJar,
+    Query(query): Query<OAuthRedirectQueryParams>,
+) -> impl IntoResponse {
+    let code = match query {
+        OAuthRedirectQueryParams::Failure { error } => {
+            return Err((
+                StatusCode::FAILED_DEPENDENCY,
+                format!("error from google: {error}"),
+            ));
+        }
+        OAuthRedirectQueryParams::Success { code, state } => {
+            if let Some(cookie) = jar.get(COOKIE_NAME)
+                && cookie.value() == state.secret()
+            {
+                code
+            } else {
+                return Err((
+                    StatusCode::BAD_REQUEST,
+                    "CSRF token does not match oauth state".to_string(),
+                ));
+            }
+        }
+    };
+
+    let token_response = google_oauth
+        .0
+        .exchange_code(code)
+        .request_async(&reqwest_client)
+        .await
+        .unwrap();
+
+    let acquired_at = Timestamp::now();
+    let Some(expires_at) = token_response
+        .expires_in()
+        .map(|expires_in| acquired_at + expires_in)
+    else {
+        return Err((
+            StatusCode::FAILED_DEPENDENCY,
+            "offline authorization missing expires_in".to_string(),
+        ));
+    };
+
+    let access_token = token_response.access_token().clone();
+    let Some(refresh_token) = token_response.refresh_token().cloned() else {
+        return Err((
+            StatusCode::FAILED_DEPENDENCY,
+            "offline authorization should always provide a refresh token".to_string(),
+        ));
+    };
+
+    let token_state = TokenState {
+        acquired_at,
+        expires_at,
+        access_token,
+        refresh_token,
+    };
+
+    Ok((jar.remove(COOKIE_NAME), Redirect::to("/")))
 }
--- token-manager-server/src/main.rs
+++ token-manager-server/src/main.rs
@@ -1,27 +1,158 @@
-use std::{env, path::PathBuf, sync::Arc, time::Duration};
+use std::{
+    env,
+    io::{ErrorKind, Read, Write},
+    ops::Deref,
+    path::{Path, PathBuf},
+    sync::Arc,
+    time::Duration,
+};
 
 use anyhow::Context;
-use axum::Router;
-use listenfd::{self, ListenFd};
-use oauth2::{
-    AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl, basic::BasicClient, reqwest::get,
+use atomic_write_file::AtomicWriteFile;
+use axum::{Router, extract::FromRef, routing::get};
+use axum_extra::extract::cookie::Key;
+use chacha20poly1305::{
+    ChaCha20Poly1305, KeyInit as _, Nonce,
+    aead::{Aead as _, Generate as _},
 };
-use serde_derive::Deserialize;
-use tokio::fs;
+use listenfd::{self, ListenFd};
+use oauth2::{AccessToken, ClientId, ClientSecret, RefreshToken, reqwest};
+use serde_derive::{Deserialize, Serialize};
+use time::Timestamp;
+use tokio::{
+    fs::{self, File},
+    io::AsyncReadExt as _,
+    sync::{Mutex, MutexGuard},
+};
 
-use crate::activity_tracker::{ActivityTracker, idle_tracking_middleware, watchdog};
+use crate::{
+    activity_tracker::{ActivityTracker, idle_tracking_middleware, watchdog},
+    auth::google::{self, GoogleOauthClient, complete_youtube_oauth, start_youtube_oauth},
+};
 
 mod activity_tracker;
 mod auth;
 
 struct AppState {
-    youtube: TokenState,
-    spotify: TokenState,
+    path: PathBuf,
+    key: chacha20poly1305::Key,
+    data: Mutex<AppStateOnDisk>,
 }
 
+#[derive(Deserialize, Serialize)]
+struct AppStateOnDisk {
+    youtube: Option<TokenState>,
+    spotify: Option<TokenState>,
+}
+
+#[derive(Deserialize, Serialize)]
 struct TokenState {
-    refresh_token: Option<String>,
-    access_token: String,
+    /// The initial acquisition timestamp
+    acquired_at: Timestamp,
+
+    /// The current expiration of the access token
+    expires_at: Timestamp,
+
+    access_token: AccessToken,
+    refresh_token: RefreshToken,
+}
+
+impl AppState {
+    pub async fn insert_youtube_token(&self, token_state: TokenState) {
+        let mut data = self.data.lock().await;
+        data.youtube = Some(token_state);
+    }
+
+    pub async fn get_youtube_token(
+        &self,
+        refresh: impl AsyncFn(&TokenState) -> anyhow::Result<Option<TokenState>>,
+    ) -> anyhow::Result<Option<AccessToken>> {
+        let mut data = self.data.lock().await;
+
+        let Some(youtube) = &mut data.youtube else {
+            return Ok(None);
+        };
+
+        if youtube.expires_at > (Timestamp::now() + Duration::from_mins(1)) {
+            return Ok(Some(youtube.access_token.clone()));
+        }
+
+        data.youtube = refresh(youtube)
+            .await
+            .context("should refresh youtube token")?;
+
+        Self::save_to_disk(&self.key, &self.path, &data).context("saving new state to file")?;
+
+        Ok(data.youtube.as_ref().map(|yt| yt.access_token.clone()))
+    }
+
+    fn save_to_disk(
+        key: &chacha20poly1305::Key,
+        path: &Path,
+        data: &MutexGuard<'_, AppStateOnDisk>,
+    ) -> anyhow::Result<()> {
+        tokio::task::block_in_place(|| {
+            let app_state =
+                toml::to_string(data.deref()).expect("app state should be serializable to toml");
+
+            let cipher = ChaCha20Poly1305::new(key);
+            let nonce = Nonce::generate(); // MUST be unique per message
+            let ciphertext = cipher
+                .encrypt(&nonce, app_state.as_bytes())
+                .expect("plaintext should not be too long");
+
+            let mut file =
+                AtomicWriteFile::open(path).context("app state file should be opened")?;
+
+            file.write_all(nonce.as_slice())
+                .context("writing app state nonce")?;
+            file.write_all(&ciphertext)
+                .context("writing app state cyphertext")?;
+
+            file.commit().context("file should be comitted")?;
+
+            Ok(())
+        })
+    }
+
+    pub async fn load_from_disk(path: PathBuf, key: chacha20poly1305::Key) -> anyhow::Result<Self> {
+        let mut file = match File::open(&path).await {
+            Ok(file) => file,
+            Err(error) if error.kind() == ErrorKind::NotFound => {
+                return Ok(Self {
+                    path,
+                    key,
+                    data: Mutex::new(AppStateOnDisk {
+                        spotify: None,
+                        youtube: None,
+                    }),
+                });
+            }
+            Err(error) => {
+                return Err(error).context("app state file should be opened");
+            }
+        };
+
+        let mut nonce = [0; size_of::<Nonce>()];
+        file.read_exact(&mut nonce)
+            .await
+            .context("reading app state nonce");
+        let nonce = Nonce::from(nonce);
+
+        let mut ciphertext = Vec::new();
+        file.read_to_end(&mut ciphertext)
+            .await
+            .context("reading app state cyphertext")?;
+
+        let cipher = ChaCha20Poly1305::new(&key);
+        let plaintext = cipher.decrypt(&nonce, ciphertext.as_slice())?;
+
+        Ok(Self {
+            path,
+            key,
+            data: Mutex::new(toml::from_slice(&plaintext).context("app state should be valid")?),
+        })
+    }
 }
 
 #[derive(Deserialize)]
@@ -41,6 +172,8 @@ async fn main() -> anyhow::Result<()> {
     let credentials = PathBuf::from(
         env::var("CREDENTIALS_DIRECTORY").context("env CREDENTIALS_DIRECTORY should be set")?,
     );
+    let app_state_file =
+        PathBuf::from(env::var("APP_STATE_FILE").context("env APP_STATE_FILE should be set")?);
     let oauth_config = toml::from_str::<OauthConfig>(
         &fs::read_to_string(credentials.join("oauth-config.toml"))
             .await
@@ -60,26 +193,13 @@ async fn main() -> anyhow::Result<()> {
 
     let file_key = chacha20poly1305::Key::try_from(&master_encryption_key[..32])
         .expect("32 bits should be the length for chacha20poly1305");
-    let axum_key = axum_extra::extract::cookie::Key::from(&master_encryption_key[32..96]);
+    let cookie_key = axum_extra::extract::cookie::Key::from(&master_encryption_key[32..96]);
 
-    let hostname =
+    let oauth_root =
         env::var("OAUTH_REDIRECT_ROOT").context("env OAUTH_REDIRECT_ROOT should be set")?;
 
-    let youtube_client = BasicClient::new(oauth_config.youtube.client_id)
-        .set_client_secret(oauth_config.youtube.client_secret)
-        .set_auth_uri(
-            AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())
-                .expect("auth url should be a valid url"),
-        )
-        .set_token_uri(
-            TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string())
-                .expect("token url should be a valid url"),
-        )
-        // Set the URL the user will be redirected to after the authorization process.
-        .set_redirect_uri(
-            RedirectUrl::new(format!("{hostname}/oauth/youtube"))
-                .context("redirect url should be a valid url")?,
-        );
+    let google_oauth = google::oauth_client(oauth_config.youtube, &oauth_root)
+        .context("google oauth client should be configured")?;
 
     let mut listenfd = ListenFd::from_env();
     let web_listener = listenfd
@@ -98,10 +218,22 @@ async fn main() -> anyhow::Result<()> {
 
     tokio::spawn(watchdog(tracker.clone()));
 
+    let reqwest_client = reqwest::Client::new();
+
     axum::serve(
         web_listener,
         Router::new()
-            .route("/login/youtube", get())
+            .route("/login/youtube", get(start_youtube_oauth))
+            .route("/oauth/google", get(complete_youtube_oauth))
+            .route("/token/youtube", get(|| async {}))
+            .route("/login/spotify", get(|| async {}))
+            .route("/oauth/spotify", get(|| async {}))
+            .route("/token/spotify", get(|| async {}))
+            .with_state(RouterState {
+                cookie_key,
+                google_oauth,
+                reqwest_client,
+            })
             .layer(axum::middleware::from_fn_with_state(
                 tracker.clone(),
                 idle_tracking_middleware,
@@ -115,4 +247,17 @@ async fn main() -> anyhow::Result<()> {
     .unwrap();
 
     Ok(())
+}
+
+#[derive(Clone)]
+struct RouterState {
+    cookie_key: Key,
+    google_oauth: GoogleOauthClient,
+    reqwest_client: reqwest::Client,
+}
+
+impl FromRef<RouterState> for Key {
+    fn from_ref(state: &RouterState) -> Self {
+        state.cookie_key.clone()
+    }
 }
